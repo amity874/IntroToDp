f0cb12b39b31f2a27fa125159e28215f8f5d9acc
Amit <amity552000@gmail.com>
2021-10-11 23:45:17 +0530

    Target Sum Leetcode

--- Lecture6/targetSumLeetcode.cpp
+++ Lecture6/targetSumLeetcode.cpp
@@ -13,10 +13,38 @@ int main(int argc, char const *argv[]){
     for(int i=0;i<n;i++){
         sum+=arr[i];
     }
-    if(sum<s){
+    if(sum<s||(sum+s)%2!=0 ||(sum+s)<0){
         return 0;
     }
     ll m=(sum+s)/2;
-    std::vector<std::vector<ll>>dp(n+1,std::vector<ll>(sum+1));
+    std::vector<std::vector<ll>>dp(n+1,std::vector<ll>(m+1));
+    for(int i=0;i<dp.size();i++){
+        for(int j=0;j<dp[0].size();j++){
+            if(i==0 && j==0){
+                dp[i][j]=1;
+            }
+            else if(j==0){
+                dp[i][j]=1;
+            }
+            else if(i==0){
+                dp[i][j]=0;
+            }
+            else{
+                if(arr[i-1]<=j){
+                    dp[i][j]=dp[i-1][j]+dp[i-1][j-arr[i-1]];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+    }
+    for(int i=0;i<dp.size();i++){
+        for(int j=0;j<dp[0].size();j++){
+            std::cout<<dp[i][j]<<" ";
+        }
+        std::cout<<"\n";
+    }
+    std::cout<<dp[n][m];
 return 0;
 }
